911e685b722a03594dd275950fbebde7509b36ae
fatmanuralbayrak12992 <afatmanur924@gmail.com>
2024-08-03 20:00:34 +0300

    Create kodland1.py

---
+++ kodland1.py
@@ -0,0 +1,14 @@
+my_words = ["Hi!","Python Pro","kodland]
+print(my_words[1])
+
+#name = input()
+#print('Hİ',name)
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+
+hakkimdaki_gercekler = ["basketbol","kitap okuma","cig köfte"]
+#import random 
+hakkimdaki_gercek = random.choice(hakkimdaki_gercek)
+print(hakkimdaki_gercek)
